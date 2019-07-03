6b352b1072938abc42e7f3546c169583d76edeb0
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2019-07-03 04:47:33 -0500

    Yunikon: call java files from xperience folder

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -21,7 +21,7 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, java) \
-    $(call all-java-files-under, java_lineage)
+    $(call all-java-files-under, java_xperience)
 
 LOCAL_USE_AAPT2 := true
 
